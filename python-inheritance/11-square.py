5f29f95948ce9cc73990f1f5739ee70cea5ac4fa
amodallal <ahmadmodallal87@gmail.com>
2025-04-10 14:56:15 +0300

    Update 11-square.py

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+"""module definition"""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
+    """class definition"""
     def __init__(self, size):
         self.integer_validator(size, size)
         super().__init__(size, size)
