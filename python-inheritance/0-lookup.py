733cecdae41754f21e5f2097bee276facd6c4dce
amodallal <ahmadmodallal87@gmail.com>
2025-04-10 14:48:48 +0300

    Update 0-lookup.py

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Module that defines the function lookup for introspection"""
 def lookup(obj):
     """ Returns the list of available attributes and methods of an object """
 
