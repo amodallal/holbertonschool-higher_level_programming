0e10066e2954047f0236b6d0468b28120383fb5a
amodallal <ahmadmodallal87@gmail.com>
2025-04-10 14:56:49 +0300

    Update 4-inherits_from.py

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module definition"""
 def inherits_from(obj, a_class):
+    """function definition"""
     if isinstance(obj, a_class) and \
        issubclass(a_class, obj.__class__) is False:
         return True
