9905fd9eacce750a6ab3dde38f0bd04d646f12d8
amodallal <ahmadmodallal87@gmail.com>
2025-04-16 09:11:12 +0300

    Update 2-append_write.py

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""Module"""
 def append_write(filename="", text=""):
+    """Function"""
     with open(filename, mode='a', encoding='utf-8') as f:
         return f.write(text)
