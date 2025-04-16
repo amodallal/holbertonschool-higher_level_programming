cb701571532d01d0e5fde4118651623782a750f7
amodallal <ahmadmodallal87@gmail.com>
2025-04-16 10:27:04 +0300

    Update 7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""Module"""
 from os import path
 from sys import argv
+"""Module"""
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
