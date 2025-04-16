4dcc1bf11f5105eef8aa1cc9c945868ad7c7f657
amodallal <ahmadmodallal87@gmail.com>
2025-04-16 10:29:08 +0300

    Update 7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""This module handles saving command-line arguments to a JSON file."""
+
 from os import path
 from sys import argv
-"""Module"""
+
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
