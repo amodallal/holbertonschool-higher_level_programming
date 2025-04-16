0f2f48bae2d3bf8ad552f4f96ed3211168fe2630
amodallal <ahmadmodallal87@gmail.com>
2025-04-16 10:24:08 +0300

    Update 6-load_from_json_file.py

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""Module"""
 from json import loads
 
 
 def load_from_json_file(filename):
+    """Function"""
     with open(filename, encoding='utf-8') as f:
         return loads(f.read())
