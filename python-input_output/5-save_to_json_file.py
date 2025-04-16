c9698db90e70edd5a3e9ab789f0b37caecb47e6e
amodallal <ahmadmodallal87@gmail.com>
2025-04-16 10:21:52 +0300

    Update 5-save_to_json_file.py

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""Module"""
 from json import dumps
 
 
 def save_to_json_file(my_obj, filename):
+    """Function"""
     with open(filename, mode='w', encoding='utf-8') as f:
         f.write(dumps(my_obj))
