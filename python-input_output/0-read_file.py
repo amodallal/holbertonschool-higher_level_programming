6cf3f350b33cb4e442b901113f05b26b9864c433
amodallal <ahmadmodallal87@gmail.com>
2025-04-16 09:06:24 +0300

    Update 0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+"""Module"""
 def read_file(filename=""):
+    """Function"""
     with open(filename, encoding='utf-8') as f:
         for line in f:
             print(line, end='')
